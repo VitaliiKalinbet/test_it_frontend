de9abb1c41d6c06d3eea3eef70cf174a6507ae21
nataliasabadysh <nataliasabadysh@gmail.com>
2019-02-15 23:21:38 +0400

    added header

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,33 +1,40 @@
 // Core
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Particles from 'react-particles-js';
 
 // Instruments
+import {home} from '../../languages/ru/Home'
 import style from './Home.module.css'
 
+const part = {
+	"particles": {
+		"number": {
+			"value": 100
+		},
+		"size": {
+			"value": 2
+		}
+	},
+	"interactivity": {
+		"events": {
+			"onhover": {
+				"enable": true,
+				"mode": "repulse"
+			}
+		}
+	}
+}
 const Home = () => {
   return (
     <div className={style.Home}>
-    <Particles
-    params={{
-	    "particles": {
-	        "number": {
-	            "value": 50
-	        },
-	        "size": {
-	            "value": 3
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            "onhover": {
-	                "enable": true,
-	                "mode": "repulse"
-	            }
-	        }
-	    }
-	}} />
-      
+
+    <div className={style.title}>
+         <h1> {home.title}</h1>
+          <p>{home.post}</p>
+          <Link to='/instruction' className={style.btn}>Начать тестирование</Link>
+    </div>
+    <Particles params={part} />
     </div>
   );
 };
