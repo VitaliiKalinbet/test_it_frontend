a6e055c81aadbc4e98c6a3f07d67de0d8ed70139
nataliasabadysh <nataliasabadysh@gmail.com>
2019-02-15 22:24:35 +0400

    added header

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,32 @@
+// Core
 import React from 'react';
+import Particles from 'react-particles-js';
+
+// Instruments
 import style from './Home.module.css'
 
 const Home = () => {
   return (
-    <div className={style.test}>
+    <div className={style.Home}>
+    <Particles
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 50
+	        },
+	        "size": {
+	            "value": 3
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        }
+	    }
+	}} />
       
     </div>
   );
