7b74e983705d1e821642c4dd5c6ae6c05b9da3c2
nataliasabadysh <nataliasabadysh@gmail.com>
2019-02-16 15:40:19 +0400

    components styles

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -4,17 +4,20 @@ import { Pie } from 'react-chartjs-2';
 // COmponents
 import Task from './Task';
 
+// Instruments
 import style from './Result.module.css';
+import qa from './qa.json';
 
 const title = ['Результаты тестирования'];
+
 const options = {
-  maintainAspectRatio: false,
-  responsive: true,
+  // maintainAspectRatio: false,
+  // responsive: true,
   legend: {
     position: 'bottom',
     labels: {
       boxWidth: 20,
-      padding: 90,
+      padding: 30,
       fontSize: 16
       
     }
@@ -24,10 +27,11 @@ const options = {
  export default class Result extends Component {
   state = {
       dataPie: {
-        labels: [	'FRONT-END','BACK-END',	'QA'	],
+        labels: [	'FRONT-END','BACK-END',	'QA', 'MENEGER'	],
         datasets: [ {
-            data: [60, 30, 50],
-            backgroundColor: [ '#75ffff','#a44deb','#f5a623']
+            data: [15, 30, 25, 10],
+            backgroundColor: [ '#75ffff','#a44deb','#f5a623', '#f3a82d51'],
+            hoverBackgroundColor: [ '#75ffff','#a44deb','#f5a623', '#f3a82d51']
           } ]
       },
   }
@@ -39,8 +43,7 @@ const options = {
               <h1 className={style.title}>{title}</h1>
               <Pie data={dataPie} options={options}/>
             </div>
-
-            <Task />
+            <Task data={qa} />
           </div>
     );
   }
