4166605a3040bf60f6620e2a41630d3e338e7a88
VitaliiKalinbet <bosssvit@gmail.com>
2019-03-11 19:19:23 +0200

    Updates

---
+++ precache-manifest.4ee933001038cecb12d14cac43a7d3af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f5ce27564945d2c9a10ef827549a78c",
+    "url": "/static/media/facebook.8f5ce275.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e001e2b683bbe7ae7ad7",
+    "url": "/static/js/main.e001e2b6.chunk.js"
+  },
+  {
+    "revision": "fde98352419440928f5a",
+    "url": "/static/js/2.fde98352.chunk.js"
+  },
+  {
+    "revision": "e001e2b683bbe7ae7ad7",
+    "url": "/static/css/main.422d7957.chunk.css"
+  },
+  {
+    "revision": "fde98352419440928f5a",
+    "url": "/static/css/2.6b185332.chunk.css"
+  },
+  {
+    "revision": "603a107f4662f167784ab04b714bfea0",
+    "url": "/index.html"
+  }
+];
